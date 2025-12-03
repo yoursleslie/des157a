172ee80a80363b75653a7eb6939bc357177223f9
Leslie Navarro <lesnavarro@gmail.com>
2025-12-02 16:29:03 -0800

    12/2

--- gameon/v1/script.js
+++ gameon/v1/script.js
@@ -4,9 +4,17 @@
     console.log('reading js');
 
 
-  
-
+    const start = document.querySelector ('#start');
 
     
+    const middle = document.querySelector ('#middle');
+    const end = document.querySelector ('#end');
+   
+
+
+    const fnum = document.querySelector ('#finalNUm')
+   const number = Math.floor(Math.random() * 100);
+
+   fnum.innerHTML = `<h2> ${number} </h2>`
 
 }()));
