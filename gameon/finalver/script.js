59d01d8b0e9a53cbf6dc67e22dd3697111393269
Leslie Navarro <lesnavarro@gmail.com>
2025-12-03 21:49:03 -0800

    12/3

--- gameon/finalver/script.js
+++ gameon/finalver/script.js
@@ -4,6 +4,7 @@
 
     const startBtn = document.querySelector('#startgame');
     const gameDiv = document.querySelector('#game');
+    const wowSound = new Audio("wow.mp3");
 
     let secretNumber = 0;
     let currentPlayer = 1;
@@ -11,12 +12,24 @@
     startBtn.addEventListener('click', startGame);
 
     function startGame() {
+
+        const bgMusic = new Audio("music.mp3");
+        bgMusic.loop = true;
+        bgMusic.volume = 0.4;
+        bgMusic.play();
+
         startBtn.textContent = "Start Over";
 
         secretNumber = Math.floor(Math.random() * 100) + 1;
         currentPlayer = 1;
 
         console.log("Secret number:", secretNumber);
+
+       
+        const bgmusic = document.querySelector('#bgmusic');
+        if (bgmusic) {
+            bgmusic.play();
+        }
 
         gameDiv.innerHTML = `
             <h3 id="feedback"></h3>
@@ -42,23 +55,25 @@
                 return;
             }
 
-            // ❗ WIN CONDITION
+            
             if (guess === secretNumber) {
+                wowSound.play(); 
                 gameDiv.innerHTML = `
                     <h1>Player ${currentPlayer} WINS!</h1>
                     <h1>The number was ${secretNumber}</h1>
                 `;
+                  
                 return;
             }
 
-            // Too high/too low
+            
             if (guess < secretNumber) {
                 feedback.textContent = "Too low!";
             } else {
                 feedback.textContent = "Too high!";
             }
 
-            // Switch players
+            
             currentPlayer = currentPlayer === 1 ? 2 : 1;
             turnDisplay.textContent = `Player ${currentPlayer} — guess a number 1–100`;
 
@@ -68,6 +83,7 @@
     }
 
 })();
+
 
 
 
